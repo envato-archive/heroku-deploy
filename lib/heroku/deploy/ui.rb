3028911a1c7c0005318876a8dbeea841a7a93593
Keith Pitt <me@keithpitt.com>
2013-03-06 23:17:28 +1100

    Refacted the UI module

--- lib/heroku/deploy/ui.rb
+++ lib/heroku/deploy/ui.rb
@@ -1,21 +1,40 @@
 module Heroku::Deploy
   module UI
+    COLORS = %w(cyan yellow green magenta red)
+    COLOR_CODES = {
+      "red"     => 31,
+      "green"   => 32,
+      "yellow"  => 33,
+      "magenta" => 35,
+      "cyan"    => 36,
+    }
+
     def finish(message)
       ok(message)
       exit 0
     end
 
     def error(message)
-      puts "\033[31m#{message}\033[0m"
+      print_and_colorize message, COLOR_CODES['red']
       exit 1
     end
 
     def info(message)
-      puts "\033[36m#{message}\033[0m"
+      print_and_colorize message, COLOR_CODES['cyan']
     end
 
     def ok(message)
-      puts "\033[32m#{message}\033[0m"
+      print_and_colorize message, COLOR_CODES['green']
+    end
+
+    def banner(message)
+      print_and_colorize message, COLOR_CODES['magenta']
+    end
+
+    private
+
+    def print_and_colorize(message, color)
+      puts "\033[#{color}m#{message}\033[0m"
     end
   end
 end
