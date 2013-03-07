84ee713bd12447e0cec66d7e9bcd97997cae1f4c
Keith Pitt <me@keithpitt.com>
2013-03-08 01:33:05 +1100

    Make sure to include the shell when stealing the manifest.

--- lib/heroku/deploy/tasks/steal_manifest.rb
+++ lib/heroku/deploy/tasks/steal_manifest.rb
@@ -1,11 +1,15 @@
 module Heroku::Deploy::Task
   class StealManifest < Base
+    include Heroku::Deploy::Shell
+
     def before_push
       manifest_url = "http://#{app.host}/assets/manifest.yml"
       asset_path = "public/assets"
 
-      shell %{mkdir -p "#{asset_path}"}
-      shell %{cd "#{asset_path}" && curl -O "#{manifest_url}"}
+      task "Stealing manifest from #{colorize manifest_url, :cyan}" do
+        shell %{mkdir -p "#{asset_path}"}
+        shell %{cd "#{asset_path}" && curl -O "#{manifest_url}"}
+      end
     end
   end
 end
