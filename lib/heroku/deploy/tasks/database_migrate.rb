7d3c9605ee72b53097d121676a4b6a737cdded27
Keith Pitt <me@keithpitt.com>
2013-03-08 02:07:31 +1100

    Rewrote the migration task.

--- lib/heroku/deploy/tasks/database_migrate.rb
+++ lib/heroku/deploy/tasks/database_migrate.rb
@@ -1,32 +1,13 @@
 module Heroku::Deploy::Task
   class DatabaseMigrate < Base
-    def git
-      @git ||= Git.new
-    end
+    include Heroku::Deploy::Shell
 
     def perform
-      remote_database_shell %{rake db:migrate}, :exec => true
-    end
+      env_vars = app.env.dup
+      env_vars['RAILS_ENV'] = 'production'
 
-    private
-
-    def remote_database_shell(cmd, options = {})
-      rails_env    = config('RAILS_ENV')
-      database_url = config('DATABASE_URL')
-
-      heroku_shell %{DATABASE_URL=#{database_url} RAILS_ENV=#{rails_env} #{cmd}}, options
-    end
-
-    def heroku_shell(cmd, options={})
-      output = shell "#{cmd} --app #{app_name}", options
-
-      unless options[:exec]
-        if output.match /Authentication failure/
-          error "Arrgh! I couldnt perform this heroku command.\n#{output}"
-        end
-      end
-
-      output
+      task "Migrating the database remotely"
+      shell "bundle exec rake db:migrate", :env => env_vars, :exec => true
     end
   end
 end
