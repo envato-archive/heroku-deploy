8e3d4d55d09b1f655c3b8813e1eee41dba6ce79a
Keith Pitt <me@keithpitt.com>
2013-03-08 00:33:46 +1100

    Improved asset compilation

--- lib/heroku/deploy/tasks/compile_assets.rb
+++ lib/heroku/deploy/tasks/compile_assets.rb
@@ -6,8 +6,12 @@ module Heroku::Deploy::Task
       # TODO: Recomend that this is off?
       # initialize_on_precompile = shell %{bundle exec rails runner "puts Rails.application.config.assets.initialize_on_precompile"}
 
+      env_vars = app.env.dup
+      env_vars['RAILS_ENV'] = 'production'
+      env_vars['RAILS_GROUPS'] = 'assets'
+
       task "Precompiling assets"
-      shell "bundle exec rake assets:precompile:primary", :env => app.env, :exec => true
+      shell "bundle exec rake assets:precompile:primary", :env => env_vars, :exec => true
     end
   end
 end
