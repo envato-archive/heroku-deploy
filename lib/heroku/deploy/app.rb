12d5894a8dbf96a28af7bff254ee9c17cf104558
Keith Pitt <me@keithpitt.com>
2013-03-08 02:06:48 +1100

    Created a better API for interacting with Heroku.

--- lib/heroku/deploy/app.rb
+++ lib/heroku/deploy/app.rb
@@ -26,7 +26,36 @@ module Heroku::Deploy
       api.put_config_vars name, vars
     end
 
+    def feature_enabled?(feature)
+      all_features = api.get_features(name).body
+
+      found_feature = all_features.find { |f| f['name'].to_s == feature.to_s }
+      raise "Could not find feature `#{feature}`" unless found_feature
+
+      found_feature['enabled']
+    end
+
+    def disable_maintenance
+      post_app_maintenance '1'
+    end
+
+    def enable_maintenance
+      post_app_maintenance '1'
+    end
+
+    def enable_feature(feature)
+      api.post_feature feature, name
+    end
+
+    def disable_feature(feature)
+      api.delete_feature feature, name
+    end
+
     private
+
+    def post_app_maintenance(action)
+      api.post_app_maintenance name, action
+    end
 
     def data
       @data ||= api.get_app(name).body
