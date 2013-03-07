97efa0071938211cca20fb7753b4ece3cfbdc5a2
Keith Pitt <me@keithpitt.com>
2013-03-08 00:20:07 +1100

    Asset compilation seems to work.

--- lib/heroku/deploy/tasks/compile_assets.rb
+++ lib/heroku/deploy/tasks/compile_assets.rb
@@ -2,33 +2,12 @@ module Heroku::Deploy::Task
   class CompileAssets < Base
     include Heroku::Deploy::Shell
 
-    ENV_VARIABLES = [
-      "AWS_ACCESS_KEY_ID",
-      "AWS_SECRET_ACCESS_KEY",
-      "FOG_DIRECTORY",
-      "FOG_PROVIDER",
-      "FOG_REGION",
-      "ASSET_SYNC_GZIP_COMPRESSION",
-      "ASSET_SYNC_MANIFEST",
-      "ASSET_SYNC_EXISTING_REMOTE_FILES",
-      "RACKSPACE_USERNAME",
-      "RACKSPACE_API_KEY",
-      "GOOGLE_STORAGE_ACCESS_KEY_ID",
-      "GOOGLE_STORAGE_SECRET_ACCESS_KEY"
-    ]
-
     def before_push
-      env_variables = ENV_VARIABLES.map do |key|
-        if runner.env.has_key?(key)
-          "#{key}=#{runner.env[key].inspect}"
-        end
-      end.compact
+      # TODO: Recomend that this is off?
+      # initialize_on_precompile = shell %{bundle exec rails runner "puts Rails.application.config.assets.initialize_on_precompile"}
 
-      env_variables << "RAILS_ENV=development"
-
-      task "Precompiling assets" do
-        shell "#{env_variables.join(" ")} bundle exec rake assets:precompile"
-      end
+      task "Precompiling assets"
+      shell "bundle exec rake assets:precompile:primary", :env => runner.env, :exec => true
     end
   end
 end
