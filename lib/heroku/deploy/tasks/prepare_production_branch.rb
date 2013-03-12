52776cd89c54652d9a0a263c84b21c188115e05c
Keith Pitt <me@keithpitt.com>
2013-03-13 10:49:51 +1100

    Fixed indentation.

--- lib/heroku/deploy/tasks/prepare_production_branch.rb
+++ lib/heroku/deploy/tasks/prepare_production_branch.rb
@@ -28,7 +28,7 @@ module Heroku::Deploy::Task
         # Always hard reset to whats on origin before merging master
         # in. When we create the branch - we may not have the latest commits.
         # This ensures that we do.
-          git "reset origin/#{strategy.branch} --hard"
+        git "reset origin/#{strategy.branch} --hard"
       end
 
       task "Merging your current branch #{colorize @previous_branch, :cyan} into #{colorize strategy.branch, :cyan}" do
