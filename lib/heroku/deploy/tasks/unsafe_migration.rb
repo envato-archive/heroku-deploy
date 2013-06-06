fa23f96d79639e8744f72ae35b613e8ca37dddf9
Keith Pitt <me@keithpitt.com>
2013-06-06 11:58:03 +1000

    Revert "I don't care about preboot anymore"
    This reverts commit 314010546a7f07ea6c81710400b0e4f59289aa26.

--- lib/heroku/deploy/tasks/unsafe_migration.rb
+++ lib/heroku/deploy/tasks/unsafe_migration.rb
@@ -3,6 +3,14 @@ module Heroku::Deploy::Task
     include Heroku::Deploy::UI
 
     def before_deploy
+      task "Turn off preboot if its enabled" do
+        @preboot = app.feature_enabled? :preboot
+
+        if @preboot
+          app.disable_feature :preboot
+        end
+      end
+
       task "Turning on maintenance mode" do
         app.enable_maintenance
       end
@@ -17,6 +25,12 @@ module Heroku::Deploy::Task
     def after_deploy
       task "Turning off maintenance mode" do
         app.disable_maintenance
+      end
+
+      if @preboot
+        task "Enabling preboot again" do
+          app.enable_feature :preboot
+        end
       end
     end
   end
