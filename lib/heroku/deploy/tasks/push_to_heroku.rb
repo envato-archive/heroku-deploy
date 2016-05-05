2f559db773a2a1e34f045eb0c60c16c767ef3242
Andrei <andrewgr@users.noreply.github.com>
2016-05-05 10:46:58 +1000

    Force-push when deploying to Heroku
    This should fix a deploy error (non-fast-forward) when the deploy branch had its history rewritten.

--- lib/heroku/deploy/tasks/push_to_heroku.rb
+++ lib/heroku/deploy/tasks/push_to_heroku.rb
@@ -6,7 +6,7 @@ module Heroku::Deploy::Task
       git_url = app.git_url
 
       task "Pushing #{colorize strategy.branch, :cyan} to #{colorize "#{git_url}:master", :cyan}"
-      git "push #{git_url} #{strategy.branch}:master -v", :exec => true
+      git "push -f #{git_url} #{strategy.branch}:master -v", :exec => true
     end
   end
 end
